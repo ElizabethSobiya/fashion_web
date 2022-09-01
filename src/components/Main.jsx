6cc90b5c0bc49a7bad82ade2350d59bd3da72445
sobiya <sarockia@asu.edu>
2022-09-01 20:06:12 +0530

    trends

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Bg from '../assets/hanger.png'
 import Logo from '../assets/logo.png'
 import {RiSearchLine} from 'react-icons/ri'
@@ -6,12 +6,24 @@ import {RiSearchLine} from 'react-icons/ri'
 // search bar in the homepage
 
 function Main() {
+  const [isShown, setIsShown] = useState(false);
+
+  const handleClick = event => {
+    //  toggle shown state
+    setIsShown(current => !current);
+
+    //  or simply set it to true
+    // setIsShown(true);
+  };
+
   return (
    <>
   <div className= "bg-[rgba(215, 224, 255, 0.26)]">
     <img src={Bg} alt=""className=' opacity-75 w-full h-[100vh] object-fit:contain' />
     <div className='flex justify-evenly'>
-     <input type="text" placeholder='Search'className='absolute w-[60%] h-[8%] border-none rounded-xl focus:outline-none left-[250px] pl-10 
+     <input type="text" placeholder='Search'
+     onClick={handleClick}
+     className='absolute w-[60%] h-[8%] border-none rounded-xl focus:outline-none left-[250px] pl-10 
      text-xl font-inter  top-[120px]'   />
     <RiSearchLine size={25} color ={'#9ca3af'} className ='absolute  right-[320px] top-[135px] '/>
     </div>
@@ -19,7 +31,28 @@ function Main() {
         <img src={Logo} alt="" className='w-20 absolute right-5 top-4' />
      </div>
   </div>
-  
+  {/* 👇️ show elements on click */}
+  {isShown && (
+        <div>
+          <h1>Latest Trends</h1>
+          <div>
+            <img src="" alt="" />
+            <p>  
+            </p>
+          </div>
+          <div>
+            <h1>Popular Suggestions</h1>
+            <ul>
+              <li>Striped shirt dress</li>
+              <li>Satin shirts</li>
+              <li>Denim Jumpsuit</li>
+              <li>Leather dresses</li>
+              <li>Solid shirts</li>
+            </ul>
+          </div>
+        </div>
+      )}
+
    </>
   )
 }
