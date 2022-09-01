97c93790718e85fec7ef9fcd17d2df9f81ee5a0e
sobiya <sarockia@asu.edu>
2022-09-01 19:08:28 +0530

    border

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,7 +11,7 @@ function Main() {
   <div className= "bg-[rgba(215, 224, 255, 0.26)]">
     <img src={Bg} alt=""className=' opacity-75 w-full h-[100vh] object-fit:contain' />
     <div className='flex justify-evenly'>
-     <input type="text" placeholder='Search'className='absolute w-[60%] h-[8%] border-none rounded-md focus:outline-none left-[250px] pl-10 
+     <input type="text" placeholder='Search'className='absolute w-[60%] h-[8%] border-none rounded-xl focus:outline-none left-[250px] pl-10 
      text-xl font-inter  top-[120px]'   />
     <RiSearchLine size={25} color ={'#9ca3af'} className ='absolute  right-[320px] top-[135px] '/>
     </div>
