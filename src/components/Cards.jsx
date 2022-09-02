3d31e5cb1d08877abe57afba76f73516701cb3d5
sobiya <sarockia@asu.edu>
2022-09-02 20:47:52 +0530

    responsive

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -33,16 +33,16 @@ const heartClick = ()=> {
     {posts.map((user,id)  => (
          <div key={id} className='relative'>
             <img src={user.dress} alt=""  className='md:w-[70%] md:h-[65%] sm:w-[60%] sm:h-[55%]'/>
-            <button className='absolute md:bottom-[35%] sm:bottom-[45%] md:w-[70%] sm:w-[60%] text-white text-center bg-violet-500' >View Products</button >
+            <button className='absolute invisible group hover:visible md:bottom-[35%] sm:bottom-[45%] md:w-[70%] sm:w-[60%] text-white text-center bg-violet-500' >View Products</button >
             <RiHeart3Fill className='absolute md:top-[3%] md:right-[32%] sm:top-[3%] sm:right-[42%] text-red-600'  onClick={heartClick}/>
-            <p className='font-inter md:text-base sm:text-sm font-medium'>{user.brand_name}</p>
+            <p className='font-inter md:text-sm sm:text-xs font-medium'>{user.brand_name}</p>
            <div className='flex flex-row'>
-           <p className='text-gray-500 line-through'>Rs.{user.price} </p>
-           <span className='text-blue-400'>Rs.{user.offer_price}</span>
+           <p className='text-gray-500 line-through md:text-sm sm:text-xs '>Rs.{user.price} </p>
+           <span className='text-blue-400 md:text-sm sm:text-xs '>Rs.{user.offer_price}</span>
            </div>
            <div className='flex flex-row'>
              <Rating />  
-             <span className='pl-10'>(200)</span>
+             <span className='md:pl-5 sm:pl-4 '>(200)</span>
              </div>
         </div>
           ))}
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -5,27 +5,31 @@ import Rating from './Rating'
 function Results() {
   return (
    <>
-   <div className='flex flex-col items-start m-[3%]  md:w-[15%] sm:w-[15%]'>
-   <h1 className='text-black text-xl font-inter font-semibold'>Search Results</h1>
+   <div className='flex flex-col items-start md:m-[2%] sm:m-[2%] md:p-[1%] sm:p-[1%]  md:w-[15%] sm:w-[15%]'>
+   <p className='text-black md:text-lg sm:text-base font-inter font-semibold'>Search Results</p>
+   <br />
    <div className='leading-8'>
     <div className='flex flex-row'> 
     <h1 className='uppercase flex flex-row flex-start font-inter font-semibold md:text-sm sm:text-xs'>Brands</h1>
+    <br />
     <RiArrowDropDownLine className='flex-end'/>
     </div>
     
-    <input type="checkbox" /><label className='md:text-sm sm:text-xs'>Mango</label>
+   <div className='border-solid border-b-1 border-black-400'>
+   <input type="checkbox" /><label className='md:text-sm sm:text-xs'>Mango</label>
     <br />
     <input type="checkbox" /><label className='md:text-sm sm:text-xs' >H & M</label>
+   </div>
    
    </div>
    <div className='leading-8 md:text-sm sm:text-xs'>
    <div className='flex flex-row'> 
    <h1 className='uppercase flex flex-row flex-start font-inter font-semibold md:text-sm sm:text-xs'>Price Range</h1>
+   <br />
    <RiArrowDropDownLine/>
    </div>
-    
-    <input type="checkbox" /><label className='md:text-sm sm:text-xs'>Under 500</label>
-    <br />
+    <input type="checkbox" /><label className='md:text-sm sm:text-xs '>Under 500</label>
+    <br /> 
     <input type="checkbox" /><label className='md:text-sm sm:text-xs' >1000-3000</label>
    
    </div>
